82b49ef7ba623967898f1d1a6a805bcf090b87ce
gbrggrn <gustberg107@student.kau.se>
2025-01-23 14:38:52 +0100

    Setup model and enums.

---
+++ CSharp2_A1/Models/AnimalSpecies/SpeciesBirds/Eagle.cs
@@ -0,0 +1,13 @@
+﻿using Csharp2_A1.Models.Categories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp2_A1.Models.AnimalSpecies.SpeciesBirds
+{
+    class Eagle : Birds
+    {
+    }
+}
