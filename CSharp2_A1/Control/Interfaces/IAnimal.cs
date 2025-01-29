3bfd3b39f8b402f8c241455cb71bfa614b0ad990
gbrggrn <gustberg107@student.kau.se>
2025-01-29 19:18:38 +0100

    Started refactoring. Interfaces placed in Control and started implementation

---
+++ CSharp2_A1/Control/Interfaces/IAnimal.cs
@@ -0,0 +1,18 @@
+﻿using Csharp2_A1.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp2_A1.Control.Interfaces
+{
+    internal interface IAnimal
+    {
+        string Id { get; set; }
+        string Age { get; set; }
+        string Name { get; set; }
+        Enums.Gender Gender { get; set; }
+        bool IsDomesticated { get; set; }
+    }
+}
---
+++ CSharp2_A1/Control/Interfaces/ICategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp2_A1.Control.Interfaces
+{
+    internal interface ICategory
+    {
+        string CategoryTrait { get; set; }
+        string CategoryQuestion { get; set; }
+    }
+}
