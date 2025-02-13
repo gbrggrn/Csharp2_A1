512d9e02abe6477ab3c258ba2bdc48f7e8de757e
gbrggrn <gustberg107@student.kau.se>
2025-02-13 14:36:39 +0100

    More documentation...

--- CSharp2_A1/MainWindow.xaml.cs
+++ CSharp2_A1/MainWindow.xaml.cs
@@ -147,15 +147,15 @@ namespace CSharp2_A1
 
                     firstQTextBox.Visibility = Visibility.Visible;
                     secondQTextBox.Visibility = Visibility.Visible;
-                }
 
-                //If categories are disabled to highlight the category based on species selection
-                if (listAllCheckBox.IsChecked == true)
-                {
-                    string category = GetCorrespondingCategory(animalInterface.Animal.GetType().Name); //Get corresponding category
-                    categoryListBox.SelectionChanged -= LoadSpecies; //Remove subscription to prevent exception
-                    categoryListBox.SelectedItem = category; //Highlight the category
-                    categoryListBox.SelectionChanged += LoadSpecies; //Re-assign subscription
+                    //If categories are disabled to highlight the category based on species selection
+                    if (listAllCheckBox.IsChecked == true)
+                    {
+                        string category = GetCorrespondingCategory(animalInterface.Animal.GetType().Name); //Get corresponding category
+                        categoryListBox.SelectionChanged -= LoadSpecies; //Remove subscription to prevent exception
+                        categoryListBox.SelectedItem = category; //Highlight the category
+                        categoryListBox.SelectionChanged += LoadSpecies; //Re-assign subscription
+                    }
                 }
             }
             else
@@ -188,6 +188,9 @@ namespace CSharp2_A1
         /// <summary>
         /// Retrieves all types in the assembly, then iterates over them to filter out classnames of categories and species.
         /// Adds the categories as keys and the corresponding species as a list as values to a dictionary.
+        /// It seems to me this method might be inefficient at scale since it is iterating multiple times over the
+        /// entire assembly. I still think this is a fun approach to dynamically retrieve the class-names but
+        /// a future small side-project should be to make this more efficient...
         /// </summary>
         /// <returns>The complete dictionary of categories and corresponding species</returns>
         private Dictionary<string, List<string>> GetCategoriesAndSpecies()
@@ -195,19 +198,24 @@ namespace CSharp2_A1
             Dictionary<string, List<string>> categoriesAndSpecies = new();
             Type[] allTypes = Assembly.GetExecutingAssembly().GetTypes();
 
+            //Iterates over all types to retrieve a category and find its' corresponding species
             foreach (Type category in allTypes)
             {
                 if (category.IsClass && category.Namespace == "Csharp2_A1.Models.AnimalCategories")
                 {
+                    //Initialize list to store species for retrieved category
                     List<string> speciesList = new List<string>();
 
-                    foreach (var species in allTypes)
+                    //Iterates over all types to retrieve the species connected to the retrieved category
+                    foreach (Type species in allTypes)
                     {
                         if (species.IsClass && species.Namespace == $"Csharp2_A1.Models.AnimalSpecies.Species{category.Name}")
                         {
+                            //Add retreieved specie to the list of this category
                             speciesList.Add(species.Name);
                         }
                     }
+                    //Add the category and its' species to the dictionary
                     categoriesAndSpecies.Add(category.Name, speciesList);
                 }
             }
