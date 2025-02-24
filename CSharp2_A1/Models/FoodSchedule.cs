ef557944ed3975b2d4e27e5b1d2fbace5f47dbf2
gbrggrn <gustberg107@student.kau.se>
2025-02-24 15:30:15 +0100

    Added functionality to manipulate the FoodSchedule class

--- CSharp2_A1/Models/FoodSchedule.cs
+++ CSharp2_A1/Models/FoodSchedule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace Csharp2_A1.Models
 {
@@ -15,6 +16,49 @@ namespace Csharp2_A1.Models
         { 
             eaterType = Enums.Enums.EaterType.Unknown;
             foodList = new();
+        }
+
+        private bool AddInstruction(string instructionIn)
+        {
+            if (FoodList != null)
+            {
+                foodList.Add(instructionIn);
+                return true;
+            }
+
+            return false;
+        }
+
+        private string GetSpecificInstruction(int indexIn)
+        {
+            if (FoodList != null && FoodList.Count <= indexIn)
+            {
+                return FoodList[indexIn];
+            }
+
+            return string.Empty;
+        }
+
+        private bool EditInstruction(string newInstructionIn, int indexIn)
+        {
+            if (FoodList != null && FoodList.Count <= indexIn)
+            {
+                FoodList[indexIn] = newInstructionIn;
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool RemoveInstruction(int indexIn)
+        {
+            if (FoodList != null && FoodList.Count <= indexIn)
+            {
+                FoodList.RemoveAt(indexIn);
+                return true;
+            }
+
+            return false;
         }
         
         public Enums.Enums.EaterType EaterType
