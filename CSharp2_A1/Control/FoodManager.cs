a3d6dc614a8974919ffd21745ed0364a945bd414
gbrggrn <gustberg107@student.kau.se>
2025-03-19 13:49:53 +0100

    Implemented methods in FoodManager

--- CSharp2_A1/Control/FoodManager.cs
+++ CSharp2_A1/Control/FoodManager.cs
@@ -2,6 +2,7 @@
 using Csharp2_A1.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,39 @@ namespace Csharp2_A1.Control
 {
     internal class FoodManager : ObservableCollectionManager<FoodItem>
     {
-        
+        Dictionary<Animal, List<FoodItem>> connections;
+
+        internal FoodManager()
+        {
+            connections = new();
+        }
+
+        internal bool AddConnection(Animal animal, List<FoodItem> item)
+        {
+            try
+            {
+                connections.Add(animal, item);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBoxes.DisplayErrorBox("Something went wrong - no connection made");
+                return false;
+            }
+        }
+
+        internal bool RemoveConnection(Animal animal, FoodItem itemToRemove)
+        {
+            try
+            {
+                connections[animal].Remove(itemToRemove);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBoxes.DisplayErrorBox("Something went wrong - could not remove fooditem");
+                return false;
+            }
+        }
     }
 }
