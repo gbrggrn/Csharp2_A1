12c9fe45b7eb3812129984dd23da3213a82f7a0a
gbrggrn <gustberg107@student.kau.se>
2025-04-07 20:53:58 +0200

    Added user-defined exception-class

---
+++ CSharp2_A1/Control/UserDefinedExceptions/SerializationException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp2_A1.Control.UserDefinedExceptions
+{
+    public class SerializationException : Exception
+    {
+        public SerializationException() { }
+
+        public SerializationException(string message) : base(message) { }
+
+        public SerializationException(string message, Exception inner) : base(message, inner) { }
+
+        public SerializationException(string message, string moreInfo) : base(message)
+        {
+            MoreInfo = moreInfo;
+        }
+
+        public string MoreInfo { get; set; }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, More information: {MoreInfo}";
+        }
+    }
+}
